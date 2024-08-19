3a1411f72df8f6b093f2646c63f83e6468b02074
taesu <taesu@crscube.co.kr>
2024-08-20 08:51:24 +0900

    Initial commit

---
+++ src/test/kotlin/io/taesu/kotlinplayground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.taesu.kotlinplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
