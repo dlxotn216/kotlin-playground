33f1fa341cf4c8009e82bd395e5ad7092f7f8a97
taesu <taesu@crscube.co.kr>
2024-08-20 10:23:25 +0900

    make codec and test

---
+++ src/main/kotlin/io/taesu/kotlinplayground/Base64Codec.kt
@@ -0,0 +1,60 @@
+package io.taesu.kotlinplayground
+
+import org.slf4j.LoggerFactory
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.io.PrintWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.Path
+import java.util.*
+
+/**
+ * Created by itaesu on 2024. 8. 20..
+ *
+ * @author Lee Tae Su
+ * @version kotlin-playground
+ * @since kotlin-playground
+ */
+class Base64Codec {
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    fun getBase64File(
+        encodedFile: Path,
+        inputStream: InputStream,
+        printWriter: PrintWriter,
+    ): Path {
+        val encoder = Base64.getEncoder()
+        val bufferSize = 1024 * 3
+        val buffer = ByteArray(bufferSize)
+        var readLength: Int
+        while ((inputStream.read(buffer).also { readLength = it }) > 0) {
+            val tmp = buffer.copyOf(readLength)
+            log.info("Read length: ${readLength}, buffer size: ${buffer.size}, tmp size: ${tmp.size}")
+            printWriter.println(String(encoder.encode(tmp), StandardCharsets.UTF_8))
+        }
+        return encodedFile
+    }
+
+    fun getBase64File(
+        encodedFile: Path,
+        inputStream: BufferedInputStream,
+        printWriter: PrintWriter,
+    ): Path {
+        val encoder = Base64.getEncoder()
+        val bufferSize = 1024 * 3
+        val buffer = ByteArray(bufferSize)
+        var readLength: Int
+        while ((inputStream.read(buffer).also { readLength = it }) > 0) {
+            val tmp = buffer.copyOf(readLength)
+            log.info("Read length: ${readLength}, buffer size: ${buffer.size}, tmp size: ${tmp.size}")
+            printWriter.println(String(encoder.encode(tmp), StandardCharsets.UTF_8))
+        }
+        return encodedFile
+    }
+
+    fun decode(value: String): ByteArray {
+        val decoder = Base64.getDecoder()
+        val decoded = decoder.decode(value)
+        return decoded
+    }
+}
